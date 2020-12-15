d49c13ec5719ae4c95306c850d696b3a66985abe
Mayowa Shoyinka <mayowas29@gmail.com>
2020-12-14 13:36:23 -1100

    updated the landing-page

---
+++ scripts/report-2.js
@@ -0,0 +1,12 @@
+function submit(form){
+   var A= 5;
+   var B= 4;
+   var C= 3;
+   var D= 2;
+   var F= 0;
+   A= eval(form.A.value)
+   a= eval(form.a.value)
+   B= eval(form.B.value)
+   answer= A+B+a
+   ans.value= answer
+}
